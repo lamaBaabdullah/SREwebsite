9213daf87f7be4cf5a6e0b473ce86594393e9357
saraAlkhrayef <105759506+saraAlkhrayef@users.noreply.github.com>
2022-05-18 04:29:47 +0300

    Update program
    Update program page and function

---
+++ UpdatePFunction.php
@@ -0,0 +1,45 @@
+<?php
+
+include('connection.php');
+
+    $Pid= $_GET['id'];
+    
+    if ( empty($_POST['major']) || empty($_POST['programName']) || empty($_POST['Description']) ){
+        $_SESSION['fill'] = 'please fill all in the form';
+        header("Location: UpdateProgram.php?id=$Did");  
+    }
+    
+   
+    
+     if (!empty($_POST['major']) && !empty($_POST['programName']) && !empty($_POST['Description']) ){
+        $major = $_POST['major'] ;
+        $Description = $_POST['Description'];
+        $programName = $_POST['programName'];
+        
+       
+     
+         // Get file's name
+        $fileName = $_FILES['file']['name'];
+        // File Path (if you want to change the folder, you should create it first or you will get an error)
+        $filePath = "images/".basename($fileName);
+        
+        
+       $requestQuery = "UPDATE program SET ";
+       $requestQuery .= "program_name ='$programName'";
+       $requestQuery .= " , description='$Description'";
+        $requestQuery .= " , major='$major'";
+        
+         if(move_uploaded_file($_FILES['file']['tmp_name'], $filePath)) {
+            // I did it here with single file
+            $requestQuery .= " ,photo='$filePath' ";
+        }
+        $requestQuery .= "WHERE id='".$Pid."'";
+        $requestResult = (mysqli_query( $connection,$requestQuery ));
+    
+     }
+     
+        if ( $requestResult ){
+          $_SESSION['success'] = 'Request has been updated';
+          header("Location: UpdateProgram.php");
+        }
+?>
