1be6ea5b1d28055f9d7cbdfd0ac4e95e86f82dac
lamaBaabdullah <98309043+lamaBaabdullah@users.noreply.github.com>
2022-05-18 05:23:06 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,5 @@
 <?php
-    // Here is the include section
-    // You can define the database connection, 
-    // and change username or password from one place instead of running over the whole project's files.
+   
     
     $connection = mysqli_connect("localhost", "root", "root", "sre-4");
     $error = mysqli_connect_error();
